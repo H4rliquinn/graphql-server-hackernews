1d42e27f3be09e66d0ffac2d1caa510fac1cca3e
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-04 10:58:01 -0500

    Added db hosted in Prisma Cloud

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.hottographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  },
-  {
-    id: "link-1",
-    url: "www.sandbox.com",
-    description: "Sandbox for GraphQL"
-  }
-];
-let idCount = links.length;
+const { prisma } = require("./generated/prisma-client");
+// let links = [
+//   {
+//     id: "link-0",
+//     url: "www.hottographql.com",
+//     description: "Fullstack tutorial for GraphQL"
+//   },
+//   {
+//     id: "link-1",
+//     url: "www.sandbox.com",
+//     description: "Sandbox for GraphQL"
+//   }
+// ];
+// let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    // feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
     link: (parent, args) => {
       console.log("Sending Back ID: " + args.id);
       let retVal = "";
@@ -41,50 +44,56 @@ const resolvers = {
   //   url: parent => parent.url
   // }
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (id, args) => {
-      let retVal = "";
-      links.forEach(x => {
-        if (x.id === args.id) {
-          if (args.url != null) {
-            x.url = args.url;
-          }
-          if (args.description != null) {
-            x.description = args.description;
-          }
-          retVal = x;
-        }
+    // post: (parent, args) => {
+    //   const link = {
+    //     id: `link-${idCount++}`,
+    //     description: args.description,
+    //     url: args.url
+    //   };
+    //   links.push(link);
+    //   return link;
+    // },
+    // updateLink: (id, args) => {
+    //   let retVal = "";
+    //   links.forEach(x => {
+    //     if (x.id === args.id) {
+    //       if (args.url != null) {
+    //         x.url = args.url;
+    //       }
+    //       if (args.description != null) {
+    //         x.description = args.description;
+    //       }
+    //       retVal = x;
+    //     }
+    //   });
+    //   return retVal;
+    // },
+
+    // deleteLink: (parent, args) => {
+    //   let rem = "";
+    //   let ret = "";
+
+    //   for (let x = 0; x < links.length; x++) {
+    //     if (links[x].id === args.id) {
+    //       rem = x;
+    //       ret = links[x];
+    //     }
+    //   }
+    //   links.splice(rem, 1);
+    //   return ret;
+    // }
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
       });
-      return retVal;
-    },
-
-    deleteLink: (parent, args) => {
-      let rem = "";
-      let ret = "";
-
-      for (let x = 0; x < links.length; x++) {
-        if (links[x].id === args.id) {
-          console.log("rem=" + x);
-          rem = x;
-          ret = links[x];
-        }
-      }
-      links.splice(rem, 1);
-      return ret;
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log("Server is running on http://localhost:4000"));
