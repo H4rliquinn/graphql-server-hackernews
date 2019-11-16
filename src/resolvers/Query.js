ac9e9f59b239ad3ef8d2b7c749e6b26b7f556d92
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-15 20:02:37 -0500

    Subscriptions complete. Added list of votes query for fun

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,11 @@ function feed(parent, args, context, info) {
   return context.prisma.links();
 }
 
+function votes(parent, args, context, info) {
+  return context.prisma.votes();
+}
+
 module.exports = {
-  feed
+  feed,
+  votes
 };
